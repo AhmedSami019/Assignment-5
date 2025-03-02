3adb925671660e26446ad120650a3b92d4522563
AhmedSami019 <easami019192@gmail.com>
2025-03-02 09:45:48 +0600

    finished some section also the color changing option

--- script/script.js
+++ script/script.js
@@ -1,12 +1,71 @@
+// for generate random color for the body section
+
+document
+  .getElementById("theme-btn")
+  .addEventListener("click", function (event) {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+
+    document.getElementById(
+      "body"
+    ).style.backgroundColor = `rgb(${red}, ${green}, ${blue} )`;
+  });
+
 // call for the 1st compleat button
-getBtnClickResultById('btn-1st');
+getBtnClickResultById("btn-1st");
 // call for the 2nd compleat button
-getBtnClickResultById('btn-2nd')
+getBtnClickResultById("btn-2nd");
 // call for the 3rd compleat btn
-getBtnClickResultById('btn-3')
+getBtnClickResultById("btn-3");
 // call for the 4th compleat btn
-getBtnClickResultById('btn-4')
+getBtnClickResultById("btn-4");
 // call for the 5th compleat btn
-getBtnClickResultById('btn-5')
+getBtnClickResultById("btn-5");
 // call for the 6th compleat btn
-getBtnClickResultById('btn-6')
+getBtnClickResultById("btn-6");
+
+// for clear history
+
+// getting access in the question page
+document
+  .getElementById("discover-new")
+  .addEventListener("click", function (event) {
+    window.location.href = "../question.html";
+  });
+
+// getting the date and day in the top of the main section
+const day = document.getElementById("day-top-main");
+const date = document.getElementById("date-top-main");
+const currentDate = new Date();
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const Months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+const currentDay = weekdays[currentDate.getDay()];
+const thisDate = currentDate.getDate();
+const thisMonth = Months[currentDate.getMonth() - 1];
+const thisYear = currentDate.getFullYear();
+const outputDate = [thisDate, thisMonth, thisYear];
+
+day.innerText = currentDay;
+date.innerText = outputDate;
--- utilities/utilities.js
+++ utilities/utilities.js
@@ -23,6 +23,24 @@ function getBtnClickResultById(id){
         
         const navbarOutput = convertedNavbarCount + 1;
         navbarCount.innerText = navbarOutput
+
+        // code to append child into the history area
+        const historyArea = document.getElementById('history-area')
+        
+        const time = (new Date()).toLocaleTimeString(); 
+        const historyItem = document.createElement('p')
+        historyItem.classList.add('text-center', 'text-md', 'font-regular', 'bg-slate-200', 'mx-4', 'p-4', 'rounded-lg', 'mb-2')
+        historyItem.innerHTML = `
+            <p>You have complete this at ${time}</p>
+        `
+        historyArea.appendChild(historyItem);
     })
 }
+
+document.getElementById('clear-history-btn').addEventListener('click', function(event){
+    
+    const historyArea = document.getElementById('history-area')
+    const items = document.querySelectorAll('p')
+    historyArea.innerHTML = null;
+})
 
